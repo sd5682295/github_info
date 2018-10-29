ad0482e36e58b27396980e709f6fd3a1390fab71
icepsps <181673940@qq.com>
2018-10-29 23:35:41 +0800

    Upgrade version 0.2 & add function : str_to_int

--- week11/p1.py
+++ week11/p1.py
@@ -14,9 +14,30 @@ class qsort(object):
         self.mysort(start_index, store_index - 1)
         self.mysort(store_index, end_index)
 
+def str_to_int(k1,k2):
+    k1 = int(k1)
+    k2 = str(k2)
+    s = ((100-k1)*(26**11))
+    c = 10
+    for i in k2:
+        s += (ord(i)*(26**c))
+        c -= 1
+    return s
+
 
 if __name__ == '__main__':
-    a = [3, 2, 1, 5, 4, 8, 7, 10, 9]
-    b = qsort(a)
-    b.mysort(0, 9)
-    print(b.data)
+    # a = [3, 2, 1, 5, 4, 8, 7, 10, 9]
+    # b = qsort(a)
+    # b.mysort(0, 9)
+    # print(b.data)
+    data = [(50,'sun'),(50,'zhang'),(80,'sun'),(90,'zhang'),(90,'sun')]
+    s = []
+
+    for k1,k2 in data:
+        a = str_to_int(k1,k2)
+        s.append(a)
+    ss = qsort(s)
+    ss.mysort(0,5)
+    print(ss.data)
+
+
